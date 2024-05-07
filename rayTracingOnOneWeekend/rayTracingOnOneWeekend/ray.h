736f30698fb253fc22360644e982b3abde13e605
xuqingchun <xuqingchun@partners.poissonsoft.com>
2024-05-07 21:12:23 +0800

    image 1 boucing spheres

--- rayTracingOnOneWeekend/rayTracingOnOneWeekend/ray.h
+++ rayTracingOnOneWeekend/rayTracingOnOneWeekend/ray.h
@@ -5,10 +5,13 @@ class ray
 {
 public:
 	ray() {}
-	ray(const point3& origin, const vec3& direction) :orig(origin), dir(direction) {}
+	ray(const point3& origin, const vec3& direction) :orig(origin), dir(direction), tm(0) {}
+	ray(const point3& origin, const vec3& direction, double time) :orig(origin), dir(direction),tm(time){}
 
 	const point3& origin() const { return orig; }
 	const vec3& direction() const { return dir; }
+
+	double time() const { return tm; }
 
 	point3 at(double t) const
 	{
@@ -19,4 +22,5 @@ public:
 private:
 	point3 orig;
 	vec3 dir;
+	double tm;
 };
