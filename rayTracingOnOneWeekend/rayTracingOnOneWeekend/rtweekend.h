6c533f531c6b7e68e15d556c96bb9ac3b6000b9e
xuqingchun <xuqingchun@partners.poissonsoft.com>
2024-05-07 18:17:04 +0800

    figure 8: pixel samples

--- rayTracingOnOneWeekend/rayTracingOnOneWeekend/rtweekend.h
+++ rayTracingOnOneWeekend/rayTracingOnOneWeekend/rtweekend.h
@@ -13,7 +13,7 @@ using std::sqrt;
 
 // Constants
 
-const double infinity = std::numeric_limits<double>::infinity();
+
 const double pi = 3.1415926535897932385;
 
 // Utility Functions
@@ -25,5 +25,6 @@ inline double degrees_to_radians(double degrees) {
 // Common Headers
 
 #include "color.h"
+#include "interval.h"
 #include "ray.h"
 #include "vec3.h"
