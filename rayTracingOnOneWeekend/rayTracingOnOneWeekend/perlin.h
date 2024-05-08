81b90170b06217e7b5a06230c50228953cf7cf31
xuqingchun <xuqingchun@partners.poissonsoft.com>
2024-05-08 15:39:14 +0800

    image 11 perlin texture, trilinearly interpolated, smoothed

--- rayTracingOnOneWeekend/rayTracingOnOneWeekend/perlin.h
+++ rayTracingOnOneWeekend/rayTracingOnOneWeekend/perlin.h
@@ -30,6 +30,10 @@ public:
 		auto v = p.y() - floor(p.y());
 		auto w = p.z() - floor(p.z());
 
+		u = u * u * (3 - 2 * u);
+		v = v * v * (3 - 2 * v);
+		w = w * w * (3 - 2 * w);
+
 		auto i = int(floor(p.x()));
 		auto j = int(floor(p.y()));
 		auto k = int(floor(p.z()));
