8f294531f55a19235c20ee868ce942e4cd0a5dbd
xuqingchun <xuqingchun@partners.poissonsoft.com>
2024-05-09 14:29:54 +0800

    image 2: cornell box, with stratification

---
+++ rayTracingOnOneWeekend/rayTracingOnOneWeekend/pi.cc
@@ -0,0 +1,24 @@
+#pragma once
+#include "rtweekend.h"
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int N = 100000;
+	int inside_circle = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		auto x = random_double(-1, 1);
+		auto y = random_double(-1, 1);
+
+		if (x * x + y * y < 1) inside_circle++;
+	}
+
+	std::cout << std::fixed << std::setprecision(12);
+	std::cout << "Eistmate of Pi = " << (4.0 * inside_circle) / N << "\n";
+}
